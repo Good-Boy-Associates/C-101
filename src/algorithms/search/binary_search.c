a4f9a36723adcfbe42feddb9b026e78c7e6a1b94
Rohith04MVK <007rohitjj04@gmail.com>
2023-05-31 21:47:10 +0530

    Added binary search algorithm

---
+++ src/algorithms/search/binary_search.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+int binarySearch(int arr[], int low, int high, int target) {
+    while (low <= high) {
+        int mid = low + (high - low) / 2;
+
+        if (arr[mid] == target) {
+            return mid;
+        } else if (arr[mid] < target) {
+            low = mid + 1;
+        } else {
+            high = mid - 1;
+        }
+    }
+    return -1;
+}
+
+int main() {
+    int arr[] = {1, 2, 3, 4, 5, 6};
+    int n = sizeof(arr) / sizeof(arr[0]);
+    int target = 4;
+
+    int result = binarySearch(arr, 0, n - 1, target);
+
+    if (result == -1) {
+        printf("Element not found\n");
+    } else {
+        printf("Element found at index %d\n", result);
+    }
+
+    return 0;
+}
