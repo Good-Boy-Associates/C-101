155a99153b789b9033e05e207f449b100012b512
Rohith04MVK <rohithkbobby04@gmail.com>
2023-06-10 23:07:29 +0530

    Added a decimal to hex converter

--- src/misc/dec_to_hex.c
+++ src/misc/dec_to_hex.c
@@ -1,50 +1,29 @@
-#include <stdio.h>
-#include <string.h>
 
-// Function to convert binary to hexadecimal
-void binaryToHexadecimal(const char* binary) {
-    int decimal = 0;
-    int base = 1;
-    int binaryLength = strlen(binary);
-    
-    // Convert binary to decimal
-    for (int i = binaryLength - 1; i >= 0; i--) {
-        if (binary[i] == '1') {
-            decimal += base;
-        }
-        base *= 2;
+#include <stdio.h>
+  
+int main() {
+  
+    int decimal_Number;
+    printf("Enter a number: ");
+    scanf("%d", &decimal_Number);
+    int i = 1, j, temp;
+    char hexa_Number[100];
+
+    while (decimal_Number != 0) {
+        temp = decimal_Number % 16;
+
+        if (temp < 10)
+            temp = temp + 48;
+        else
+            temp = temp + 55;
+        hexa_Number[i++] = temp;
+        decimal_Number = decimal_Number / 16;
     }
-    
-    // Convert decimal to hexadecimal
-    char hexadecimal[16];
-    int index = 0;
-    
-    while (decimal > 0) {
-        int remainder = decimal % 16;
-        if (remainder < 10) {
-            hexadecimal[index] = remainder + '0';
-        } else {
-            hexadecimal[index] = remainder + 'A' - 10;
-        }
-        decimal /= 16;
-        index++;
-    }
-    
-    // Print the hexadecimal value in reverse order
-    printf("Hexadecimal: ");
-    for (int i = index - 1; i >= 0; i--) {
-        printf("%c", hexadecimal[i]);
+    // printing the hexa decimal number
+    printf("Hexadecimal value is: ");
+    for (j = i - 1; j > 0; j--) {
+        printf("%c", hexa_Number[j]);
     }
     printf("\n");
-}
-
-int main() {
-    char binary[32];
-    
-    printf("Enter a binary number: ");
-    scanf("%s", binary);
-    
-    binaryToHexadecimal(binary);
-    
     return 0;
 }
