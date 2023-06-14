5da3156075aa34581f2e2b84d6f36049d0d867cb
Rohith04MVK <rohithkbobby04@gmail.com>
2023-06-14 22:03:56 +0530

    Added strong number checker

---
+++ src/basic_c/strong.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+int factorial(int n) {
+    int fact = 1;
+    for (int i = 1; i <= n; i++) {
+        fact *= i;
+    }
+    return fact;
+}
+
+int main() {
+    int num;
+    printf("Enter a number: ");
+    scanf("%d", &num);
+
+    int originalNum = num;
+    int sum = 0;
+
+    while (num > 0) {
+        int digit = num % 10;
+        sum += factorial(digit);
+        num /= 10;
+    }
+
+    if (sum == originalNum) {
+        printf("%d is a strong number.\n", originalNum);
+    } else {
+        printf("%d is not a strong number.\n", originalNum);
+    }
+
+    return 0;
+}
