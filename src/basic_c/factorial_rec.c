3dc27a09762f39d0e4b6437bc98221620932c852
Rohith04MVK <rohithkbobby04@gmail.com>
2023-08-03 22:27:22 +0530

    Added factorial with recursion

---
+++ src/basic_c/factorial_rec.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int factorial(int n) {
+    if (n == 0) {
+        return 1;
+    }
+    else {
+        return n * factorial(n - 1);
+    }
+}
+
+int main() {
+    int num;
+    printf("Enter a non-negative integer: ");
+    scanf("%d", &num);
+
+    if (num < 0) {
+        printf("Factorial is not defined for negative numbers.\n");
+    } else {
+        int result = factorial(num);
+        printf("Factorial of %d is %d.\n", num, result);
+    }
+
+    return 0;
+}
